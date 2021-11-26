6cc5bd9db253b048b172c629023c4f1ed8245891
Ian Torres <iantorres@outlook.com>
2021-11-25 21:43:37 -0300

    Command extract added.

---
+++ app/Console/Commands/Extract.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Goutte\Client;
+
+class Extract extends Command
+{
+    /**
+     * The Goutte Http Client.
+     *
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * Selector of titles
+     *
+     * @var string
+     */
+    protected $titles = "#sec-main > ul > li > a";
+
+
+    /**
+     * Selector of descriptions
+     *
+     * @var string
+     */
+    protected $descriptions = "#sec-main > ul > li";
+
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'owasp:extract';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Extract information from OWASP site.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->client = new Client();
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $crawler = $this->client->request('GET', 'https://owasp.org/www-project-top-ten/');
+
+        $titles = collect();
+        $descriptions = collect();
+        $data = [];
+
+        $crawler->filter($this->titles)->each(function ($node) use ($titles) {
+            $titles->push($node->text());
+        });
+
+
+        $crawler->filter($this->descriptions)->each(function ($node) use ($descriptions) {
+            $descriptions->push($node->text());
+        });
+
+
+        for ($i = 0; $i < $descriptions->count(); $i++)
+        {
+            $data[] = [
+                'title' => $titles->get($i),
+                'description' => $descriptions->get($i),
+            ];
+        }
+
+        return Command::SUCCESS;
+    }
+}
